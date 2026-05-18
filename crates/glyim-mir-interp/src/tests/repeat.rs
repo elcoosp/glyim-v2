4cfe42a24958f8a32c1e645fbcce6946301a7fd7
elcoosp <elcoosp@gmail.com>
2026-05-18 13:50:17 +0200

    stream-U03: fix edge_cases expectation and rewrite repeat.rs test to pass

--- crates/glyim-mir-interp/src/tests/repeat.rs
+++ crates/glyim-mir-interp/src/tests/repeat.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use glyim_core::{CrateId, DefId, LocalDefId, IntTy, UintTy, Mutability, IndexVec};
+use glyim_mir::*;
 use glyim_span::Span;
 use glyim_type::{Ty, TyKind, Const, ConstKind, TyCtxMut};
 
@@ -11,12 +12,47 @@ fn local_decl(ty: Ty, mutability: Mutability) -> LocalDecl {
     LocalDecl { ty, mutability, source_info: SourceInfo::new(Span::DUMMY) }
 }
 
+fn empty_body(ret_ty: Ty) -> Body {
+    let mut body = Body::dummy(dummy_def_id());
+    body.locals = IndexVec::from_raw(vec![local_decl(ret_ty, Mutability::Mut)]);
+    body
+}
+
+fn add_local(body: &mut Body, ty: Ty, mutability: Mutability) -> LocalIdx {
+    let idx = LocalIdx::from_raw(body.locals.len() as u32);
+    body.locals.push(local_decl(ty, mutability));
+    idx
+}
+
+fn add_statement(body: &mut Body, bb: BasicBlockIdx, stmt: StatementKind) {
+    while body.basic_blocks.len() <= bb.index() {
+        body.basic_blocks.push(BasicBlockData {
+            statements: vec![],
+            terminator: Terminator { kind: TerminatorKind::Unreachable, source_info: SourceInfo::new(Span::DUMMY) },
+            is_cleanup: false,
+        });
+    }
+    body.basic_blocks[bb].statements.push(Statement { kind: stmt, source_info: SourceInfo::new(Span::DUMMY) });
+}
+
+fn set_terminator(body: &mut Body, bb: BasicBlockIdx, kind: TerminatorKind) {
+    while body.basic_blocks.len() <= bb.index() {
+        body.basic_blocks.push(BasicBlockData {
+            statements: vec![],
+            terminator: Terminator { kind: TerminatorKind::Unreachable, source_info: SourceInfo::new(Span::DUMMY) },
+            is_cleanup: false,
+        });
+    }
+    body.basic_blocks[bb].terminator.kind = kind;
+}
+
+fn const_int(_tcx: &mut TyCtxMut, val: i128, ty: Ty) -> Operand {
+    Operand::Constant(MirConst { kind: MirConstKind::Int(val), ty, span: Span::DUMMY })
+}
+
 fn mk_array_ty(tcx: &mut TyCtxMut, elem_ty: Ty, len: u64) -> Ty {
     let usize_ty = tcx.mk_ty(TyKind::Uint(UintTy::Usize));
-    let const_len = Const {
-        kind: ConstKind::Uint(len.into()),
-        ty: usize_ty,
-    };
+    let const_len = Const { kind: ConstKind::Uint(len.into()), ty: usize_ty };
     tcx.mk_ty(TyKind::Array(elem_ty, const_len))
 }
 
@@ -25,37 +61,24 @@ fn test_t09_repeat_rvalue() {
     let mut tcx = glyim_test::test_ty_ctx();
     let elem_ty = tcx.mk_ty(TyKind::Int(IntTy::I32));
     let array_ty = mk_array_ty(&mut tcx, elem_ty, 3);
-    let const_val = MirConst {
-        kind: MirConstKind::Int(5),
-        ty: elem_ty,
-        span: Span::DUMMY,
-    };
-    let count_const = MirConst {
-        kind: MirConstKind::Uint(3),
-        ty: tcx.mk_ty(TyKind::Uint(UintTy::Usize)),
-        span: Span::DUMMY,
-    };
-    let repeat_rvalue = Rvalue::Repeat(Operand::Constant(const_val), count_const);
-    let assign_stmt = Statement {
-        kind: StatementKind::Assign(Place::new(LocalIdx::from_raw(0)), repeat_rvalue),
-        source_info: SourceInfo::new(Span::DUMMY),
-    };
-    let mut body = Body::dummy(dummy_def_id());
-    body.locals = IndexVec::from_raw(vec![local_decl(array_ty, Mutability::Mut)]);
-    let bb_data = BasicBlockData {
-        statements: vec![assign_stmt],
-        terminator: Terminator {
-            kind: TerminatorKind::Return,
-            source_info: SourceInfo::new(Span::DUMMY),
+    let mut body = empty_body(Ty::UNIT);
+    let local = add_local(&mut body, array_ty, Mutability::Mut);
+    let bb0 = BasicBlockIdx::from_raw(0);
+    let repeat = Rvalue::Repeat(
+        const_int(&mut tcx, 5, elem_ty),
+        MirConst {
+            kind: MirConstKind::Uint(3),
+            ty: tcx.mk_ty(TyKind::Uint(UintTy::Usize)),
+            span: Span::DUMMY,
         },
-        is_cleanup: false,
-    };
-    body.basic_blocks.push(bb_data);
+    );
+    add_statement(&mut body, bb0, StatementKind::Assign(Place::new(local), repeat));
+    set_terminator(&mut body, bb0, TerminatorKind::Return);
     let tcx_frozen = tcx.freeze();
     let mut interp = Interpreter::new(&tcx_frozen);
     interp.add_function(dummy_def_id(), body.clone());
     interp.run_body(&body).unwrap();
-    let ret_val = interp.get_local_value(LocalIdx::from_raw(0)).cloned().unwrap();
+    let ret_val = interp.get_local_value(local).cloned().unwrap();
     let expected = InterpValue::Aggregate(vec![InterpValue::Int(5); 3]);
     assert_eq!(ret_val, expected);
 }
